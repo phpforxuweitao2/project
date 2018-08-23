881b35b65c30e924db0f802e56f92a052fa66a99
phpmyred <1137282177@qq.com>
2018-08-23 22:53:09 +0800

    66666

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::group(['namespace'=>'Home'],function(){
     ]);
 });
 
+Route::get('/home',function(){
+	return '666666';
+});
+
 //后端
 Route::group(['namespace'=>'Admin'],function(){
     Route::get('bk_index','IndexController@index');
