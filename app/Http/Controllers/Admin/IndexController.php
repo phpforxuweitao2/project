cb8a4d70cf0d2bbb0fceedd291a3f1843b7fbd42
phpforxuweitao <xuweitao1139204319@foxmail.com>
2018-08-23 22:34:49 +0800

    添加一个后台IndexController

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+/*  User: tao    Date: 2018/8/23   Time: 22:33  */
+
+
+namespace App\Http\Controllers\Admin;
+
+
+class IndexController
+{
+    public function index() {
+        return 'Admin->IndexController->index()';
+    }
+}
