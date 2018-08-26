bf898dd66208fb9932f0aac68e75bf5d799f7f2f
phpforxuweitao <xuweitao1139204319@foxmail.com>
2018-08-26 16:06:58 +0800

    公共尾部文件

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -12,11 +12,6 @@ class IndexController
     public function index() {
 
         return view('admin.index.index');
-<<<<<<< HEAD
-=======
-
-
->>>>>>> d233a2629bc7570f6ed3fb21cbb8b0ae689af4d1
 
     }
 }
